d9eed9307db240cf05bdf382ca8e917da4ed0bf2
Evil Buddy <xavier.douarre@gmail.com>
2022-01-21 15:58:12 +0100

    Updated the main page
    It's now super cool WITH MUSIC :3

--- GLH.js
+++ GLH.js
@@ -1,3 +1,10 @@
 function ld(page) {
     window.open("./" + page + "/index.html", "_self")
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+	var mus = document.getElementById("music");
+
+	mus.currentTime = 24.2;
+	mus.play();
+});
